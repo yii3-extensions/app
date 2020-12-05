01da95c2ac659dafb19c258c21d48281cdd615f4
Wilmer Arambula <terabytesoftw@gmail.com>
2020-12-05 12:29:00 -0300

    Fix workflow with code coverage.

--- public/index-test.php
+++ public/index-test.php
@@ -13,6 +13,12 @@ use Yiisoft\Yii\Web\Application;
 use Yiisoft\Yii\Web\SapiEmitter;
 use Yiisoft\Yii\Web\ServerRequestFactory;
 
+$c3 = dirname(__DIR__) . '/c3.php';
+
+if (is_file($c3)) {
+    require_once $c3;
+}
+
 // PHP built-in server routing.
 if (PHP_SAPI === 'cli-server') {
     // Serve static files as is.
@@ -22,12 +28,6 @@ if (PHP_SAPI === 'cli-server') {
 
     // Explicitly set for URLs with dot.
     $_SERVER['SCRIPT_NAME'] = '/index-test.php';
-}
-
-$c3 = dirname(__DIR__) . '/c3.php';
-
-if (is_file($c3)) {
-    require_once $c3;
 }
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
