dad028257c6d9b27ead0aaa684681514f31c7698
Wilmer Arambula <terabytesoftw@gmail.com>
2020-12-21 10:37:57 -0300

    Fix config CacheInterface::class.

--- config/common/yiisoft-cache.php
+++ config/common/yiisoft-cache.php
@@ -11,9 +11,9 @@ use Yiisoft\Factory\Definitions\Reference;
 
 return [
     CacheInterface::class => [
-        '__class' => ArrayCache::class,
+        '__class' => Cache::class,
         '__construct()' => [
-            Reference::to(FileCache::class)
+            Reference::to(ArrayCache::class)
         ]
     ],
 ];
