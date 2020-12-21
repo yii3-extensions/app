d8bbf1f9f0771e4b8c7b0e73dfd7ca74bef37ab0
Wilmer Arambula <terabytesoftw@gmail.com>
2020-12-21 10:29:41 -0300

    Fix config cache.

--- config/common/yiisoft-cache.php
+++ config/common/yiisoft-cache.php
@@ -2,16 +2,16 @@
 
 declare(strict_types=1);
 
+use Yiisoft\Cache\ArrayCache;
 use Yiisoft\Cache\Cache;
 use Yiisoft\Cache\CacheInterface;
-use Yiisoft\Cache\File\FileCache;
 use Yiisoft\Factory\Definitions\Reference;
 
 /** @var array $params */
 
 return [
     CacheInterface::class => [
-        '__class' => Cache::class,
+        '__class' => ArrayCache::class,
         '__construct()' => [
             Reference::to(FileCache::class)
         ]
