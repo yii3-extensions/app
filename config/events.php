0e1ba69a734fb6e48506eb46a5d1ce4d1b6a9963
Wilmer Arambula <terabytesoftw@gmail.com>
2020-12-23 10:13:48 -0300

    Add ReverseBlockMerge::class.

--- config/events.php
+++ config/events.php
@@ -6,8 +6,10 @@ use App\Event\EventMessageSent;
 use App\Event\EventMessageNotSent;
 use Yii\Extension\Service\Event\MessageSent;
 use Yii\Extension\Service\Event\MessageNotSent;
+use Yiisoft\Arrays\Modifier\ReverseBlockMerge;
 
 return [
     MessageSent::class => [[EventMessageSent::class, 'addFlash']],
     MessageNotSent::class => [[EventMessageNotSent::class, 'addFlash']],
+    ReverseBlockMerge::class => new ReverseBlockMerge(),
 ];
