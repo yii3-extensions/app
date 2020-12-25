2844c1a7bddfadac1aa036a40d56f55eee29006f
Wilmer Arambula <terabytesoftw@gmail.com>
2020-12-25 14:55:36 -0300

    Fixed tests codeception.

--- tests/Acceptance/PageErrorCest.php
+++ tests/Acceptance/PageErrorCest.php
@@ -8,16 +8,12 @@ use App\Tests\AcceptanceTester;
 
 final class PageErrorCest
 {
-    public function _before(AcceptanceTester $I): void
-    {
-        $I->amOnPage('/error');
-    }
-
     public function errorPage(AcceptanceTester $I): void
     {
+        $I->amOnPage('/about');
         $I->wantTo('see error page.');
         $I->see('404');
-        $I->see('The page /error not found.');
+        $I->see('The page /about not found.');
         $I->see('The above error occurred while the Web server was processing your request.');
         $I->see('Please contact us if you think this is a server error. Thank you.');
     }
--- tests/Acceptance/PageIndexCest.php
+++ tests/Acceptance/PageIndexCest.php
@@ -8,13 +8,9 @@ use App\Tests\AcceptanceTester;
 
 final class PageIndexCest
 {
-    public function _before(AcceptanceTester $I): void
-    {
-        $I->amOnPage('/');
-    }
-
     public function indexPage(AcceptanceTester $I): void
     {
+        $I->amOnPage('/');
         $I->wantTo('see page index.');
         $I->see('Hello World');
     }
