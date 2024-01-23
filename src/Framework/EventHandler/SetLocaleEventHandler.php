636483df6b150042666fc902b3612c30ec7bc0a9
Wilmer Arambula <terabytesoftw@gmail.com>
2024-01-23 07:36:08 -0300

    Fix style-ci 2.

--- src/Framework/EventHandler/SetLocaleEventHandler.php
+++ src/Framework/EventHandler/SetLocaleEventHandler.php
@@ -10,9 +10,7 @@ use Yiisoft\Yii\Middleware\Event\SetLocaleEvent;
 
 final class SetLocaleEventHandler
 {
-    public function __construct(private readonly TranslatorInterface $translator, private readonly WebView $webView)
-    {
-    }
+    public function __construct(private readonly TranslatorInterface $translator, private readonly WebView $webView) {}
 
     public function handle(SetLocaleEvent $event): void
     {
--- src/Framework/Handler/NotFoundHandler.php
+++ src/Framework/Handler/NotFoundHandler.php
@@ -19,8 +19,7 @@ final class NotFoundHandler implements RequestHandlerInterface
         private readonly CurrentRoute $currentRoute,
         private readonly ViewRenderer $viewRenderer,
         private readonly string $viewPath = __DIR__ . '/view',
-    ) {
-    }
+    ) {}
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
