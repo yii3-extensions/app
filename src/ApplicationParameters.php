ba6ff68c9db77cf00d4d4f77229cfc6a38388b7a
Wilmer Arambula <terabytesoftw@gmail.com>
2024-01-26 07:07:06 -0300

    Adopt last changes `php-forge/html`.

--- src/ApplicationParameters.php
+++ src/ApplicationParameters.php
@@ -260,17 +260,14 @@ final class ApplicationParameters
     private function dropdownToggle(): ElementInterface
     {
         return ButtonToggle::widget()
-            ->class(
-                'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-            )
-            ->dataDropdownToggle(true)
+            ->class('text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800')
+            ->dataDropdownToggle('selector-language')
             ->id('selector-language-toggle')
             ->toggleContent(
                 Svg::widget($this->getSvgDefinitions())
                     ->filePath($this->aliases->get('@fontawesome-free/svgs/solid/globe.svg'))
                     ->height(20)
-            )
-            ->toggleId('selector-language');
+            );
     }
 
     private function menuToggle(): ElementInterface
