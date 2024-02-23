32208fa33c8000617a6eb0218f1077647e40a55e
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2024-02-23 16:50:05 -0300

    Use `php-forge/html` stable. (#38)

--- src/ApplicationParameters.php
+++ src/ApplicationParameters.php
@@ -131,7 +131,7 @@ final class ApplicationParameters
             'containerClass()' => ['hidden w-full md:block md:w-auto'],
             'listClass()' => ['font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'],
             'linkClass()' => ['block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'],
-            'toggleButton()' => [$this->menuToggle()],
+            'toggle()' => [$this->menuToggle()],
         ];
     }
 
@@ -206,7 +206,7 @@ final class ApplicationParameters
             )
             ->linkClass('block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white')
             ->listclass('py-2 text-sm text-gray-700 dark:text-gray-200')
-            ->toggleButton($this->dropdownToggle());
+            ->toggle($this->dropdownToggle());
     }
 
     public function getToggleTheme(): ElementInterface
@@ -276,7 +276,6 @@ final class ApplicationParameters
             ->ariaExpanded('false')
             ->class('inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600')
             ->dataCollapseToggle('navbar-default')
-            ->icon(true)
             ->id(null)
             ->toggleContent(
                 Span::widget()->class('sr-only')->content('Open main menu'),
